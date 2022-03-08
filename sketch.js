51bbd120948895f7ce580b6fe5257208c7947c5a
gargi0709 <84441268+gargi0709@users.noreply.github.com>
2022-03-08 14:10:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var astronaut1, astronaut2
+var goal1, goal2,
+var background, backgroundimg
+var ball
+
+function preload(){
+  backgroundimg = loadImage("images/mars.png")
+  astronaut1 = loadImage("image/astronaut 1.png")
+  astronaut2 = loadImage("image/astronaut mirrior.png")
+  goal1 = loadImage("image/goal 1.png")
+  goal2 = loadImage("image/goal mirrior.png")
+  ball = loadImage("image/ballimg.png")
+}
